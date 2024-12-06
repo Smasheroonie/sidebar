72bb70f4a82b4f75081afbdec0efc3a0070a0dd9
Luke_G_WSL <68129402+Smasheroonie@users.noreply.github.com>
2024-12-06 22:10:53 +0000

    complete sidebar styling

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -9,10 +9,11 @@ import {
   MoreHoriz,
 } from "@mui/icons-material";
 import SidebarLink from "./SidebarLink";
+import { Button } from "@mui/material";
 
 const Sidebar = () => {
   return (
-    <div className="w-64 min-w-64 p-5 mt-5 mr-auto mb-0 ml-auto shadow-md shadow-blue-300">
+    <div className="w-fit min-w-64 pt-5 pr-3 pl-3 pb-3 mt-5 mr-auto mb-0 ml-auto shadow-md shadow-blue-300">
       <h1 className="font-semibold text-2xl m- ml-3">Sidebar</h1>
 
       <SidebarLink text="Home" Icon={Home} />
@@ -23,6 +24,7 @@ const Sidebar = () => {
       <SidebarLink text="Lists" Icon={List} />
       <SidebarLink text="Profile" Icon={AccountBox} />
       <SidebarLink text="More" Icon={MoreHoriz}/>
+      <Button className="w-full h-12" variant="contained" sx={{"border-radius": "20px", "font-weight": "700", "background-color": "blue", "margin-top": "5px"}}>Chuck</Button>
     </div>
   );
 };
--- src/SidebarLink.jsx
+++ src/SidebarLink.jsx
@@ -1,8 +1,8 @@
 const SidebarLink = ({ text, Icon }) => {
   return (
-    <div className="flex justify-start cursor-pointer rounded-3xl hover:bg-sky-100 hover:text-blue-600 hover:transition-colors ease-out duration-300">
-      {Icon && <Icon />}
-      <h2 className="font-bold text-xl my-4 ml-3">{text}</h2>
+    <div className="flex my-0.5 items-center cursor-pointer rounded-3xl hover:bg-sky-100 hover:text-blue-600 hover:transition-colors ease-out duration-300">
+      {Icon && <Icon className="ml-3" />}
+      <h2 className="font-bold text-xl my-4 ml-2">{text}</h2>
     </div>
   );
 };
